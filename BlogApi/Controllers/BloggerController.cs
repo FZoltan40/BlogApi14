b748d12faf104c62c4f4d84e958de26abc077fe8
FZoltan <farkaszoltan28@gmail.com>
2026-09-17 18:59:35 +0200

    Blogger model is created.

---
+++ BlogApi/Controllers/BloggerController.cs
@@ -0,0 +1,53 @@
+﻿using BlogApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+
+namespace BlogApi.Controllers
+{
+    [Route("blogger")]
+    [ApiController]
+    public class BloggerController : ControllerBase
+    {
+        public string ConnectionString = "server=localhost;database=blog14f;uid=root;password=;";
+
+        [HttpGet]
+        public object GetAllBlogger()
+        {
+            var bloggers = new List<Blogger>();
+
+            var connector = new MySqlConnection(ConnectionString);
+
+            connector.Open();
+
+            string sql = "SELECT * FROM blogger;";
+
+            var cmd = new MySqlCommand(sql, connector);
+
+            var datareader = cmd.ExecuteReader();
+
+            while (datareader.Read())
+            {
+                var blogger = new Blogger
+                {
+                    Id = datareader.GetInt32(0),
+                    Name = datareader.GetString(1),
+                    Email = datareader.GetString(2),
+                    Age = datareader.GetInt32(3),
+                    Password = datareader.GetString(4),
+                    RegistrationTime = datareader.GetDateTime(5)
+                };
+
+                bloggers.Add(blogger);
+            }
+
+            connector.Close();
+
+
+            return new
+            {
+                message = "Sikeres lekérdezés",
+                result = bloggers
+            };
+        }
+    }
+}
